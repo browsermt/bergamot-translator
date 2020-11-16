f8c9a6b0cce845ab8b6e1fe929cb7a6b260c72a4
Abhishek Aggarwal <aaggarwal@mozilla.com>
2020-11-16 13:17:53 +0100

    Added an application showing usage of bergamot translator
     - 'app' folder contains the application
     - The application uses dummy requests and responses for now

---
+++ app/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * main.cpp
+ *
+ * An example application to demonstrate the use of Bergamot translator.
+ *
+ */
+
+#include <iostream>
+
+#include "TranslationModelConfiguration.h"
+#include "AbstractTranslationModel.h"
+#include "TranslationRequest.h"
+#include "TranslationResult.h"
+
+
+int main(int argc, char** argv) {
+
+	// Create an instance of AbstractTranslationModel with a dummy model configuration
+	TranslationModelConfiguration config("dummy_modelFilePath",
+				"dummy_sourceVocabPath",
+				"dummy_targetVocabPath");
+	std::shared_ptr<AbstractTranslationModel> model =
+			AbstractTranslationModel::createInstance(config);
+
+	// Call to translate a dummy (empty) texts with a dummy (empty) translation request
+	TranslationRequest req;
+	std::vector<std::string> texts;
+	auto result = model->translate(std::move(texts), req);
+
+	// Resolve the future and get the actual result
+	std::vector<TranslationResult> res = result.get();
+
+	std::cout << "Count is: " << res.size() << std::endl;
+	return 0;
+}

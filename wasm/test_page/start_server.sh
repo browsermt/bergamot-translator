b81cba7e70c5637c3d7da55eaaea1e0d7df7a686
Motin <motin@motin.eu>
2021-02-14 15:17:29 +0200

    Use emrun instead of missing node.js server script from #22

--- wasm/test_page/start_server.sh
+++ wasm/test_page/start_server.sh
@@ -5,4 +5,4 @@ cp ../../build-wasm/wasm/bergamot-translator-worker.js .
 cp ../../build-wasm/wasm/bergamot-translator-worker.wasm .
 cp ../../build-wasm/wasm/bergamot-translator-worker.worker.js .
 npm install
-node bergamot-httpserver.js
+emrun --serve_after_close bergamot.html

695510aef4ec858fa71e331aef06937ac9a66bab
Jerin Philip <jphilip@ed.ac.uk>
2021-03-22 17:16:17 +0000

    Another ifdef to hide pcqueue header file

--- src/translator/service.h
+++ src/translator/service.h
@@ -4,10 +4,13 @@
 #include "batch_translator.h"
 #include "batcher.h"
 #include "data/types.h"
-#include "pcqueue.h"
 #include "response.h"
 #include "text_processor.h"
 #include "translator/parser.h"
+
+#ifdef WITH_PTHREADS
+#include "pcqueue.h"
+#endif
 
 #include <queue>
 #include <vector>
@@ -15,45 +18,54 @@
 namespace marian {
 namespace bergamot {
 
+/// Service exposes methods to translate an incoming blob of text to the
+/// Consumer of bergamot API.
+///
+/// An example use of this API looks as follows:
+///
+///  options = ...;
+///  service = Service(options);
+///  std::string input_blob = "Hello World";
+///  std::future<Response>
+///      response = service.translate(std::move(input_blob));
+///  response.wait();
+///  Response result = response.get();
 class Service {
 
-  // Service exposes methods to translate an incoming blob of text to the
-  // Consumer of bergamot API.
-  //
-  // An example use of this API looks as follows:
-  //
-  //  options = ...;
-  //  service = Service(options);
-  //  std::string input_blob = "Hello World";
-  //  std::future<Response>
-  //      response = service.translate(std::move(input_blob));
-  //  response.wait();
-  //  Response result = response.get();
-
 public:
-  /**
-   * @param options Marian options object
-   * @param model_memory byte array (aligned to 64!!!) that contains the bytes
-   * of a model.bin. Optional, defaults to nullptr when not used
-   */
+  /// @param options Marian options object
+  /// @param model_memory byte array (aligned to 64!!!) that contains the bytes
+  /// of a model.bin. Optional, defaults to nullptr when not used
   explicit Service(Ptr<Options> options, const void *model_memory = nullptr);
 
+  /// Construct Service from a string configuration.
+  /// @param [in] config string parsable as YAML expected to adhere with marian
+  /// config
   explicit Service(const std::string &config,
                    const void *model_memory = nullptr)
       : Service(parseOptions(config), model_memory) {}
 
-  // Convenience accessor methods to extract these vocabulary outside service.
-  // e.g: For use in decoding histories for marian-decoder replacement.
+  /// Shared pointer to source-vocabulary.
   Ptr<Vocab const> sourceVocab() const { return vocabs_.front(); }
+
+  /// Shared pointer to target vocabulary.
   Ptr<Vocab const> targetVocab() const { return vocabs_.back(); }
 
+  /// To stay efficient and to refer to the string for alignments, expects the
+  /// ownership be moved through std::move(..)
+  ///
+  ///  @param [in] rvalue of string to be translated.
   std::future<Response> translate(std::string &&input);
 
   ~Service();
 
 private:
+  /// Launches a blocking translator without using std::thread
   void initialize_blocking_translator(Ptr<Options> options);
+  /// Translates through direct interaction between batcher_ and translators_
   void blocking_translate();
+
+  /// Launches multiple workers of translators using std::thread
   void initialize_async_translators(Ptr<Options> options);
   void async_translate();
 

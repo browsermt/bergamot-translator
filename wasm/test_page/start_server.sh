4112f28cfb724e0acbf9b0ed9c8f3f28b7e00b74
Abhishek Aggarwal <aaggarwal@mozilla.com>
2021-03-24 14:18:45 +0100

    Refactor the code that patches wasm artifacts to enable wormhole

--- wasm/test_page/start_server.sh
+++ wasm/test_page/start_server.sh
@@ -4,13 +4,6 @@ cp ../../build-wasm/wasm/bergamot-translator-worker.data .
 cp ../../build-wasm/wasm/bergamot-translator-worker.js .
 cp ../../build-wasm/wasm/bergamot-translator-worker.wasm .
 cp ../../build-wasm/wasm/bergamot-translator-worker.worker.js .
-echo "Done----"
-
-echo "Start: Enabling wormhole via APIs that compile and instantiate wasm module-------"
-sed -i.bak 's/var result = WebAssembly.instantiateStreaming(response, info);/var result = WebAssembly.instantiateStreaming(response, info, {simdWormhole:true});/g' bergamot-translator-worker.js
-sed -i.bak 's/return WebAssembly.instantiate(binary, info);/return WebAssembly.instantiate(binary, info, {simdWormhole:true});/g' bergamot-translator-worker.js
-sed -i.bak 's/var module = new WebAssembly.Module(bytes);/var module = new WebAssembly.Module(bytes, {simdWormhole:true});/g' bergamot-translator-worker.js
-echo "Done: Enabling wormhole via APIs that compile and instantiate wasm module--------"
 
 npm install
 echo "Start httpserver"

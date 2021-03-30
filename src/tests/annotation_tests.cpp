2a2e1326c049105d3d938cca99e172e2b4af5fe4
Jerin Philip <jphilip@ed.ac.uk>
2021-03-30 17:41:06 +0000

    text.text in test app renamed

--- src/tests/annotation_tests.cpp
+++ src/tests/annotation_tests.cpp
@@ -17,23 +17,23 @@ TEST_CASE("Test Annotation API with random sentences") {
   std::mt19937 randomIntGen_;
   randomIntGen_.seed(42);
 
-  AnnotatedText text;
+  AnnotatedText testAnnotation;
   std::vector<std::vector<ByteRange>> sentenceWords;
   std::vector<ByteRange> Words;
 
   for (size_t idx = 0; idx < sentences; idx++) {
     if (idx != 0)
-      text.text += "\n";
+      testAnnotation.text += "\n";
 
     Words.clear();
     size_t words = randomIntGen_() % maxWords + 1;
     Words.reserve(words);
     for (size_t idw = 0; idw < words; idw++) {
-      size_t before = text.text.size();
+      size_t before = testAnnotation.text.size();
       std::string word = std::to_string(idx) + "-" + std::to_string(idw);
-      text.text += word;
+      testAnnotation.text += word;
       if (idw != 0)
-        text.text += " ";
+        testAnnotation.text += " ";
       Words.push_back((ByteRange){before, before + word.size() - 1});
     }
     // std::cout << std::endl;
@@ -46,12 +46,12 @@ TEST_CASE("Test Annotation API with random sentences") {
   for (auto &sentence : sentenceWords) {
     std::vector<marian::string_view> wordByteRanges;
     for (auto &word : sentence) {
-      marian::string_view wordView(&text.text[word.begin],
+      marian::string_view wordView(&testAnnotation.text[word.begin],
                                    word.end - word.begin);
       wordByteRanges.push_back(wordView);
       // std::cout << std::string(wordView) << " ";
     }
-    text.addSentence(wordByteRanges);
+    testAnnotation.addSentence(wordByteRanges);
     byteRanges.push_back(wordByteRanges);
     // std::cout << std::endl;
   }
@@ -60,13 +60,13 @@ TEST_CASE("Test Annotation API with random sentences") {
   for (int idx = 0; idx < sentenceWords.size(); idx++) {
     for (int idw = 0; idw < sentenceWords[idx].size(); idw++) {
       ByteRange expected = sentenceWords[idx][idw];
-      ByteRange obtained = text.wordAsByteRange(idx, idw);
-      // std::cout << std::string(text.word(idx, idw)) << " ";
+      ByteRange obtained = testAnnotation.wordAsByteRange(idx, idw);
+      // std::cout << std::string(testAnnotation.word(idx, idw)) << " ";
       CHECK(expected.begin == obtained.begin);
       CHECK(expected.end == obtained.end);
 
       std::string expected_string = std::string(byteRanges[idx][idw]);
-      CHECK(expected_string == std::string(text.word(idx, idw)));
+      CHECK(expected_string == std::string(testAnnotation.word(idx, idw)));
     }
     // std::cout << std::endl;
   }

e1b47193119753342871dbde647c004d11241c8e
Jerin Philip <jphilip@ed.ac.uk>
2021-04-06 19:31:08 +0000

    Revert "Stronger unit tests on sentences + num words, num sentences"
    This reverts commit a315dc0ff6c00b614448925c92448b9dc22a9340.

--- src/tests/annotation_tests.cpp
+++ src/tests/annotation_tests.cpp
@@ -14,131 +14,60 @@ TEST_CASE("Test Annotation API with random sentences") {
   size_t sentences = 20;
   size_t maxWords = 40;
 
-  bool debug{false}; // Set in case needed to see output.
-
   std::mt19937 randomIntGen_;
   randomIntGen_.seed(42);
 
   AnnotatedText testAnnotation;
   std::vector<std::vector<ByteRange>> sentenceWords;
-  std::vector<ByteRange> wordByteRanges;
-  std::vector<ByteRange> sentenceByteRanges;
+  std::vector<ByteRange> Words;
 
-  // Prepare the text and construct ByteRanges as intended for sentences and
-  // words. The ByteRanges we construct here are expected to be the
-  // ground-truths for words and sentences.
   for (size_t idx = 0; idx < sentences; idx++) {
     if (idx != 0)
       testAnnotation.text += "\n";
 
-    wordByteRanges.clear();
+    Words.clear();
     size_t words = randomIntGen_() % maxWords + 1;
-    wordByteRanges.reserve(words);
-    size_t sentenceBegin, sentenceEnd;
+    Words.reserve(words);
     for (size_t idw = 0; idw < words; idw++) {
-      if (idw != 0)
-        testAnnotation.text += " ";
       size_t before = testAnnotation.text.size();
       std::string word = std::to_string(idx) + "-" + std::to_string(idw);
       testAnnotation.text += word;
-      wordByteRanges.push_back((ByteRange){before, before + word.size()});
-
-      if (idw == 0) {
-        sentenceBegin = before;
-      }
-      if (idw == words - 1) {
-        sentenceEnd = before + word.size();
-      }
+      if (idw != 0)
+        testAnnotation.text += " ";
+      Words.push_back((ByteRange){before, before + word.size() - 1});
     }
-    if (debug) {
-      std::cout << std::endl;
-    }
+    // std::cout << std::endl;
 
-    sentenceWords.push_back(wordByteRanges);
-    sentenceByteRanges.push_back((ByteRange){sentenceBegin, sentenceEnd});
+    sentenceWords.push_back(Words);
   }
 
-  // Use the AnnotatedText's addSentence() API to add sentences to transparently
-  // convert from string_views to ByteRanges, rebasing/working out the math
-  // underneath.
-  if (debug) {
-    std::cout << "Inserting words onto container:" << std::endl;
-  }
-
-  ///
-  std::vector<std::vector<marian::string_view>> wordStringViews;
+  // std::cout << "Inserting words:" << std::endl;
+  std::vector<std::vector<marian::string_view>> byteRanges;
   for (auto &sentence : sentenceWords) {
     std::vector<marian::string_view> wordByteRanges;
     for (auto &word : sentence) {
       marian::string_view wordView(&testAnnotation.text[word.begin],
-                                   word.size());
+                                   word.end - word.begin);
       wordByteRanges.push_back(wordView);
-      if (debug) {
-        std::cout << std::string(wordView) << " ";
-      }
+      // std::cout << std::string(wordView) << " ";
     }
     testAnnotation.addSentence(wordByteRanges);
-    wordStringViews.push_back(wordByteRanges);
-    if (debug) {
-      std::cout << std::endl;
-    }
+    byteRanges.push_back(wordByteRanges);
+    // std::cout << std::endl;
   }
 
-  // Access from the sentence(sentenceIdx) API and confirm that the ground truth
-  // we expect is same as what comes out of the contianer.
-  std::vector<marian::string_view> sentenceStringViews;
-  for (auto &sentenceByteRange : sentenceByteRanges) {
-    marian::string_view sentenceView(
-        &testAnnotation.text[sentenceByteRange.begin],
-        sentenceByteRange.size());
-    sentenceStringViews.push_back(sentenceView);
-  }
-
-  if (debug) {
-    std::cout << "From container: " << std::endl;
-    std::cout << "Sentences: " << std::endl;
-  }
-  for (int idx = 0; idx < sentenceByteRanges.size(); idx++) {
-    ByteRange expected = sentenceByteRanges[idx];
-    ByteRange obtained = testAnnotation.sentenceAsByteRange(idx);
-    if (debug) {
-      std::cout << std::string(testAnnotation.sentence(idx)) << " ";
-    }
-    CHECK(expected.begin == obtained.begin);
-    CHECK(expected.end == obtained.end);
-    std::string expected_string = std::string(sentenceStringViews[idx]);
-    std::string obtained_string = std::string(testAnnotation.sentence(idx));
-    CHECK(expected_string == obtained_string);
-  }
-
-  /// Access the word(sentenceIdx, wordIdx) API and confirm what we hold as
-  /// expected words are the same as those obtained.
-  if (debug) {
-    std::cout << "From container: " << std::endl;
-    std::cout << "Words: " << std::endl;
-  }
-
-  CHECK(sentenceWords.size() == testAnnotation.numSentences());
-  for (int idx = 0; idx < sentenceWords.size(); idx++) {
-    CHECK(sentenceWords[idx].size() == testAnnotation.numWords(idx));
-  }
-
+  // std::cout << "From container: " << std::endl;
   for (int idx = 0; idx < sentenceWords.size(); idx++) {
     for (int idw = 0; idw < sentenceWords[idx].size(); idw++) {
       ByteRange expected = sentenceWords[idx][idw];
       ByteRange obtained = testAnnotation.wordAsByteRange(idx, idw);
-      if (debug) {
-        std::cout << std::string(testAnnotation.word(idx, idw)) << " ";
-      }
+      // std::cout << std::string(testAnnotation.word(idx, idw)) << " ";
       CHECK(expected.begin == obtained.begin);
       CHECK(expected.end == obtained.end);
 
-      std::string expected_string = std::string(wordStringViews[idx][idw]);
-      std::string obtained_string = std::string(testAnnotation.word(idx, idw));
-      CHECK(expected_string == obtained_string);
+      std::string expected_string = std::string(byteRanges[idx][idw]);
+      CHECK(expected_string == std::string(testAnnotation.word(idx, idw)));
     }
-    if (debug) {
-      std::cout << std::endl;
-    }
+    // std::cout << std::endl;
   }
 }

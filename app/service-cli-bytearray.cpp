ca48e9b646bcad0b48abc4196878a5e0ede82cb1
Jerin Philip <jphilip@ed.ac.uk>
2021-04-22 17:35:28 +0000

    Adding ResponseOptions to service-cli-bytearray

--- app/service-cli-bytearray.cpp
+++ app/service-cli-bytearray.cpp
@@ -6,20 +6,23 @@
 #include "common/definitions.h"
 #include "common/utils.h"
 #include "marian.h"
+#include "translator/byte_array_util.h"
 #include "translator/parser.h"
 #include "translator/response.h"
 #include "translator/service.h"
-#include "translator/byte_array_util.h"
 
 int main(int argc, char *argv[]) {
   auto cp = marian::bergamot::createConfigParser();
   auto options = cp.parseOptions(argc, argv, true);
 
   // Prepare memories for model and shortlist
-  marian::bergamot::AlignedMemory modelBytes = marian::bergamot::getModelMemoryFromConfig(options);
-  marian::bergamot::AlignedMemory shortlistBytes = marian::bergamot::getShortlistMemoryFromConfig(options);
+  marian::bergamot::AlignedMemory modelBytes =
+      marian::bergamot::getModelMemoryFromConfig(options);
+  marian::bergamot::AlignedMemory shortlistBytes =
+      marian::bergamot::getShortlistMemoryFromConfig(options);
 
-  marian::bergamot::Service service(options, std::move(modelBytes), std::move(shortlistBytes));
+  marian::bergamot::Service service(options, std::move(modelBytes),
+                                    std::move(shortlistBytes));
 
   // Read a large input text blob from stdin
   std::ostringstream std_input;
@@ -27,8 +30,14 @@ int main(int argc, char *argv[]) {
   std::string input = std_input.str();
   using marian::bergamot::Response;
 
+  marian::Ptr<marian::Options> responseOptions = marian::New<marian::Options>();
+  responseOptions->set<bool>("quality", true);
+  responseOptions->set<bool>("alignment", true);
+  responseOptions->set<float>("alignment-threshold", 0.2f);
+
   // Wait on future until Response is complete
-  std::future<Response> responseFuture = service.translate(std::move(input));
+  std::future<Response> responseFuture =
+      service.translate(std::move(input), responseOptions);
   responseFuture.wait();
   Response response = responseFuture.get();
 

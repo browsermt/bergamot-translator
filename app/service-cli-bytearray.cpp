ca9e6c598106a68b09d5f0af14f5320e3a6e81b8
Jerin Philip <jphilip@ed.ac.uk>
2021-04-23 10:26:09 +0000

    Undoing service-cli autoformat

--- app/service-cli-bytearray.cpp
+++ app/service-cli-bytearray.cpp
@@ -6,23 +6,20 @@
 #include "common/definitions.h"
 #include "common/utils.h"
 #include "marian.h"
-#include "translator/byte_array_util.h"
 #include "translator/parser.h"
 #include "translator/response.h"
 #include "translator/service.h"
+#include "translator/byte_array_util.h"
 
 int main(int argc, char *argv[]) {
   auto cp = marian::bergamot::createConfigParser();
   auto options = cp.parseOptions(argc, argv, true);
 
   // Prepare memories for model and shortlist
-  marian::bergamot::AlignedMemory modelBytes =
-      marian::bergamot::getModelMemoryFromConfig(options);
-  marian::bergamot::AlignedMemory shortlistBytes =
-      marian::bergamot::getShortlistMemoryFromConfig(options);
+  marian::bergamot::AlignedMemory modelBytes = marian::bergamot::getModelMemoryFromConfig(options);
+  marian::bergamot::AlignedMemory shortlistBytes = marian::bergamot::getShortlistMemoryFromConfig(options);
 
-  marian::bergamot::Service service(options, std::move(modelBytes),
-                                    std::move(shortlistBytes));
+  marian::bergamot::Service service(options, std::move(modelBytes), std::move(shortlistBytes));
 
   // Read a large input text blob from stdin
   std::ostringstream std_input;

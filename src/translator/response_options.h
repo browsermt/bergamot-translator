bd0e20f3dfff29548ac2d67fb91252db97c0d20c
Jerin Philip <jphilip@ed.ac.uk>
2021-04-26 14:55:43 +0000

    Adding documentation

--- src/translator/response_options.h
+++ src/translator/response_options.h
@@ -1,20 +1,47 @@
-#pragma once
+#ifndef SRC_BERGAMOT_RESPONSE_OPTIONS_H_
+#define SRC_BERGAMOT_RESPONSE_OPTIONS_H_
 #include <string>
 
 namespace marian {
 namespace bergamot {
-/// Options dictating how to construct a response for an input text.
 
-enum ConcatStrategy { FAITHFUL, SPACE };
-enum QualityScoreType { FREE, EXPENSIVE };
+enum ConcatStrategy {
+  /// Target text is constructed faithful to the source-text  structure.
+  FAITHFUL,
 
+  /// Target text is concatenated by a space.
+  SPACE
+};
+
+enum QualityScoreType {
+  /// Provide a free quality-score that comes with the machine-translation model
+  /// itself.
+  FREE,
+
+  /// An expensive quality-score that runs additional computations to determine
+  /// quality of an output.
+  EXPENSIVE
+};
+
+/// ResponseOptions dictate how to construct a Response for an input string of
+/// text to be translated.
 struct ResponseOptions {
-  bool qualityScores{false};
-  bool alignment{false};
+  bool qualityScores{false}; ///< Include quality-scores or not.
+  bool alignment{false};     ///< Include alignments or not.
+
+  /// Whether to include sentenceMappings or not. Alignments require
+  /// sentenceMappings and are available irrespective of this option if
+  /// `alignment=true`.
+  bool sentenceMappings{false};
+
+  /// Threshold to filter alignments into a sparse matrix. `1.0f` gives argmax.
   float alignmentThreshold{0.2f};
+
   QualityScoreType qualityScoreType{QualityScoreType::FREE};
   ConcatStrategy concatStrategy{ConcatStrategy::FAITHFUL};
 };
 
 } // namespace bergamot
 } // namespace marian
+
+#endif //  SRC_BERGAMOT_RESPONSE_OPTIONS_H_

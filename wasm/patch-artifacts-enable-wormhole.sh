1a4add19da211e31c60c1e4ca6b27f3de5235020
abhi-agg <66322306+abhi-agg@users.noreply.github.com>
2021-05-03 17:13:43 +0200

    Improve script to patch wasm artifacts and load EN->DE vocabulary in wasm test (#125)
    * Improved script that patches wasm artifacts to enable wormhole
    
     - Made the regex pattern ignore multiple whitespaces b/w words of
       the matching pattern
    
    * Fix for loading EN->DE vocabularies in wasm test page
    
     - Loading vocabularies for EN->DE was failing because of
       the new structure of bergamot-models

--- wasm/patch-artifacts-enable-wormhole.sh
+++ wasm/patch-artifacts-enable-wormhole.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Patching wasm artifacts to enable wormhole via APIs that compile and instantiate wasm module"
-sed -i.bak 's/var result = WebAssembly.instantiateStreaming(response, info);/var result = WebAssembly.instantiateStreaming(response, info, {simdWormhole:true});/g' wasm/bergamot-translator-worker.js
-sed -i.bak 's/return WebAssembly.instantiate(binary, info);/return WebAssembly.instantiate(binary, info, {simdWormhole:true});/g' wasm/bergamot-translator-worker.js
-sed -i.bak 's/var module = new WebAssembly.Module(bytes);/var module = new WebAssembly.Module(bytes, {simdWormhole:true});/g' wasm/bergamot-translator-worker.js
+sed -i.bak 's/WebAssembly.instantiateStreaming[[:space:]]*([[:space:]]*response[[:space:]]*,[[:space:]]*info[[:space:]]*)/WebAssembly.instantiateStreaming(response, info, {simdWormhole:true})/g' wasm/bergamot-translator-worker.js
+sed -i.bak 's/WebAssembly.instantiate[[:space:]]*([[:space:]]*binary[[:space:]]*,[[:space:]]*info[[:space:]]*)/WebAssembly.instantiate(binary, info, {simdWormhole:true})/g' wasm/bergamot-translator-worker.js
+sed -i.bak 's/WebAssembly.Module[[:space:]]*([[:space:]]*bytes[[:space:]]*)/WebAssembly.Module(bytes, {simdWormhole:true})/g' wasm/bergamot-translator-worker.js
 echo "Done"

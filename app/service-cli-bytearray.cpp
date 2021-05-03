4e2c1747f414c4efd252f19f97987c52fc87c337
Jerin Philip <jphilip@ed.ac.uk>
2021-05-03 15:50:47 +0000

    collapse intermediate for bytearray apps

--- app/service-cli-bytearray.cpp
+++ app/service-cli-bytearray.cpp
@@ -6,20 +6,30 @@
 #include "common/definitions.h"
 #include "common/utils.h"
 #include "marian.h"
+#include "translator/byte_array_util.h"
 #include "translator/parser.h"
 #include "translator/response.h"
+#include "translator/response_options.h"
 #include "translator/service.h"
-#include "translator/byte_array_util.h"
 
 int main(int argc, char *argv[]) {
   auto cp = marian::bergamot::createConfigParser();
   auto options = cp.parseOptions(argc, argv, true);
 
   // Prepare memories for model and shortlist
-  marian::bergamot::AlignedMemory modelBytes = marian::bergamot::getModelMemoryFromConfig(options);
-  marian::bergamot::AlignedMemory shortlistBytes = marian::bergamot::getShortlistMemoryFromConfig(options);
+  marian::bergamot::AlignedMemory modelBytes, shortlistBytes;
 
-  marian::bergamot::Service service(options, std::move(modelBytes), std::move(shortlistBytes));
+  options->set<bool>("bytearray", true);
+  options->set<bool>("check-bytearray", true);
+
+  if (options->get<bool>("bytearray")) {
+    // Load legit values into bytearrays.
+    modelBytes = marian::bergamot::getModelMemoryFromConfig(options);
+    shortlistBytes = marian::bergamot::getShortlistMemoryFromConfig(options);
+  }
+
+  marian::bergamot::Service service(options, std::move(modelBytes),
+                                    std::move(shortlistBytes));
 
   // Read a large input text blob from stdin
   std::ostringstream std_input;

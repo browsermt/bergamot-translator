053e8e113e89f8a5a590fcee763570d5c902ca97
Jerin Philip <jphilip@ed.ac.uk>
2021-05-09 20:01:35 +0000

    Struct binds for WASM

--- wasm/bindings/TranslationRequestBindings.cpp
+++ wasm/bindings/TranslationRequestBindings.cpp
@@ -13,7 +13,19 @@ using namespace emscripten;
 
 // Binding code
 EMSCRIPTEN_BINDINGS(translation_request) {
-  class_<TranslationRequest>("TranslationRequest")
-    .constructor<>()
-    ;
+  enum_<marian::bergamot::ConcatStrategy>("ConcatStrategy")
+      .value("FAITHFUL", FAITHFUL)
+      .value("SPACE", SPACE);
+
+  enum_<marian::bergamot::QualityScoreType>("QualityScoreType")
+      .value("FREE", FREE)
+      .value("EXPENSIVE", EXPENSIVE);
+
+  value_object<TranslationRequest>("TranslationRequest")
+      .field("qualityScores", &TranslationRequest::qualityScores)
+      .field("alignment", &TranslationRequest::alignment)
+      .field("alignmentThreshold", &TranslationRequest::alignmentThreshold)
+      .field("sentenceMappings", &TranslationRequest::sentenceMappings)
+      .field("qualityScoreType", &TranslationRequest::qualityScoreType)
+      .field("concatStrategy", &TranslationRequest::concatStrategy);
 }

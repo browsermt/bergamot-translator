bd00643d1de1bbe377e7062679cd475fc5afd51b
Qianqian Zhu <qianqian.zhu@hotmail.com>
2021-05-11 13:58:44 +0100

    add hashmap in load from buffer

--- src/translator/vocabs.h
+++ src/translator/vocabs.h
@@ -1,33 +1,41 @@
 #pragma once
 
+#include "common/timer.h"
+
 namespace marian {
 namespace bergamot {
 
 class VocabsGenerator {
 public:
+  VocabsGenerator(Ptr<Options> options): options_(options){}
+
   // load from buffer
-  VocabsGenerator(Ptr<Options> options, std::vector<std::shared_ptr<AlignedMemory>>&& vocabMemories) {
+  void load(std::vector<std::shared_ptr<AlignedMemory>>&& vocabMemories) {
+    // with the current setup, we need at least two vocabs: src and trg
     ABORT_IF(vocabMemories.size() < 2, "Insufficient number of vocabularies.");
     vocabs_.resize(vocabMemories.size());
+    std::unordered_map<uintptr_t, Ptr<Vocab>> vmap;  // uintptr_t holds unique keys for share_ptr<AlignedMemory>
     for (size_t i = 0; i < vocabs_.size(); i++) {
-      Ptr<Vocab> vocab = New<Vocab>(options, i);
-      vocab->loadFromSerialized(absl::string_view(vocabMemories[i]->begin(), vocabMemories[i]->size()));
-      vocabs_[i] = vocab;
+      auto m = vmap.emplace(std::make_pair(reinterpret_cast<uintptr_t>(vocabMemories[i].get()), Ptr<Vocab>()));
+      if (m.second) { // new: load the vocab
+        m.first->second = New<Vocab>(options_, i);
+        m.first->second->loadFromSerialized(absl::string_view(vocabMemories[i]->begin(), vocabMemories[i]->size()));
+      }
+      vocabs_[i] = m.first->second;
     }
   }
 
   // load from file
-  VocabsGenerator(Ptr<Options> options){
-    auto vfiles = options->get<std::vector<std::string>>("vocabs");
+  void load(const std::vector<std::string>& vocabPath){
     // with the current setup, we need at least two vocabs: src and trg
-    ABORT_IF(vfiles.size() < 2, "Insufficient number of vocabularies.");
-    vocabs_.resize(vfiles.size());
+    ABORT_IF(vocabPath.size() < 2, "Insufficient number of vocabularies.");
+    vocabs_.resize(vocabPath.size());
     std::unordered_map<std::string, Ptr<Vocab>> vmap;
     for (size_t i = 0; i < vocabs_.size(); ++i) {
-      auto m = vmap.emplace(std::make_pair(vfiles[i], Ptr<Vocab>()));
+      auto m = vmap.emplace(std::make_pair(vocabPath[i], Ptr<Vocab>()));
       if (m.second) { // new: load the vocab
-        m.first->second = New<Vocab>(options, i);
-        m.first->second->load(vfiles[i]);
+        m.first->second = New<Vocab>(options_, i);
+        m.first->second->load(vocabPath[i]);
       }
       vocabs_[i] = m.first->second;
     }
@@ -39,17 +47,20 @@ public:
 
 private:
   std::vector<Ptr<Vocab const>> vocabs_;
+  Ptr<Options> options_;
 };
 
-std::vector<Ptr<const Vocab>> createVocabs(Ptr<Options> options,
-                                           std::vector<std::shared_ptr<AlignedMemory>>&& vocabMemories){
+std::vector<Ptr<const Vocab>> loadVocabs(Ptr<Options> options,
+                                         std::vector<std::shared_ptr<AlignedMemory>>&& vocabMemories){
+  VocabsGenerator vocabsGenerator(options);
   if(!vocabMemories.empty()){
-    // load vocabs from buffer
-    return VocabsGenerator(options,std::move(vocabMemories)).getVocabs();
+    vocabsGenerator.load(std::move(vocabMemories));  // load vocabs from buffer
   }else {
     // load vocabs from file
-    return VocabsGenerator(options).getVocabs();
+    auto vocabFiles = options->get<std::vector<std::string>>("vocabs");
+    vocabsGenerator.load(vocabFiles);
   }
+  return vocabsGenerator.getVocabs();
 }
 
 } // namespace bergamot

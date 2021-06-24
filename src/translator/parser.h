a590ef0924105893da991f8095d5e33d943b9f7b
Andre Barbosa <andre.barbosa@quintoandar.com.br>
2021-06-24 01:38:44 -0300

    include quality path as an option

--- src/translator/parser.h
+++ src/translator/parser.h
@@ -29,6 +29,11 @@ inline marian::ConfigParser createConfigParser() {
   cp.addOption<std::string>("--bergamot-mode", "Bergamot Options",
                             "Operating mode for bergamot: [wasm, native, decoder]", "native");
 
+  cp.addOption<std::string>(
+    "--quality", "Bergamot Options",
+    "File considering Quality Estimation model. Supported file extensions: .txt"
+  );
+
   return cp;
 }
 

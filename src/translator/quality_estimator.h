727ed111634b3f1ed639df312b139c0e9e8930d7
Andre Barbosa <andre.barbosa@quintoandar.com.br>
2021-06-24 01:39:52 -0300

    include map subword to word method

--- src/translator/quality_estimator.h
+++ src/translator/quality_estimator.h
@@ -2,21 +2,34 @@
 #define SRC_BERGAMOT_QUALITY_ESTIMATOR_H_
 #include <iostream>
 #include <vector>
+#include <map>
 #include <string>
 #include "annotation.h"
 #include "response.h"
+#include "intgemm/intgemm.h"
+#include "intgemm/aligned.h" 
+#include "intgemm/callbacks.h"
 
 namespace marian {
 namespace bergamot {
 
 class QualityEstimator {
     private:
+        struct FeatureVector{
+            FeatureVector(float x, float y, float w, float z): 
+                feature_vector{x,y,z,w} {}
+            float feature_vector[4];
+        };
         std::vector<float> stds, means, coefficients, intercept;
+        std::vector<std::map<std::tuple<size_t, size_t>, float>> mapping;
         void initVector(std::vector<float> &emptyVector, std::string line);
+        void mapBPEToWords(Response& sentence, Words words);
+        intgemm::AlignedVector<QualityEstimator::FeatureVector>  extractFeatures(Response& sentence);
+        std::vector<int> predictWordLevel(std::vector<std::vector<float>> feature_vector);
 
     public:
         QualityEstimator(const char *file_parameters);
-        void compute_quality_scores(AnnotatedText source);
+        void compute_quality_scores(Response& sentence, Words words);
 };
 
 } // namespace bergamot

b4bef8a1e7f572a6bebd2fe0dc8fb44f473726a1
Jerin Philip <jphilip@ed.ac.uk>
2021-06-29 15:11:33 +0000

    simplifying some mess

--- src/translator/cache.h
+++ src/translator/cache.h
@@ -17,8 +17,9 @@ class LRUCache {
   typedef typename std::list<StorageItem>::iterator StorageItr;
 
   LRUCache(size_t sizeCap) : sizeCap_(sizeCap) {}
+
   void insert(const Key key, const CacheItem item) {
-    std::lock_guard<std::recursive_mutex> guard(rwMutex_);
+    std::lock_guard<std::mutex> guard(rwMutex_);
     if (storage_.size() + 1 > sizeCap_) {
       // Evict LRU
       auto p = storage_.rbegin();
@@ -31,7 +32,7 @@ class LRUCache {
   }
 
   bool fetch(const Key key, CacheItem &item) {
-    std::lock_guard<std::recursive_mutex> guard(rwMutex_);
+    std::lock_guard<std::mutex> guard(rwMutex_);
     auto mapItr = map_.find(key);
     if (mapItr == map_.end()) {
       return false;
@@ -39,7 +40,8 @@ class LRUCache {
       auto storageItr = mapItr->second;
       item = storageItr->second;
       storage_.erase(storageItr);
-      insert(key, item);
+      storage_.push_front({key, item});
+      map_.insert({key, storage_.begin()});
       return true;
     }
   }
@@ -48,7 +50,7 @@ class LRUCache {
   size_t sizeCap_;
   std::list<StorageItem> storage_;
   std::unordered_map<Key, StorageItr> map_;
-  std::recursive_mutex rwMutex_;
+  std::mutex rwMutex_;
 };
 
 typedef LRUCache<std::string, History> TranslatorLRUCache;

83cd7d1976bfee293cf6450a95e8c73d5e0a1274
Andre Barbosa <andre.barbosa@quintoandar.com.br>
2021-07-05 00:58:41 -0300

    refactor quality estimator

--- src/translator/quality_estimator.cpp
+++ src/translator/quality_estimator.cpp
@@ -23,7 +23,6 @@ void QualityEstimator::load(AlignedMemory file_parameters) {
     file_input.emplace_back(line);
   }
   ABORT_IF(file_input.size() != 4, "Model file should contains 4 lines, one per model parameter");
-
   QualityEstimator::initVector(this->stds_, file_input[0]);
   QualityEstimator::initVector(this->means_, file_input[1]);
   QualityEstimator::initVector(this->coefficients_, file_input[2]);
@@ -32,14 +31,9 @@ void QualityEstimator::load(AlignedMemory file_parameters) {
 
 void QualityEstimator::initVector(std::vector<float>& emptyVector, std::string line) {
   char delimiter = ' ';
-  size_t start = 0;
-  size_t end = line.find_first_of(delimiter);
-
-  while (end <= std::string::npos) {
-    emptyVector.push_back(std::stof(line.substr(start, end - start)));
-    if (end == std::string::npos) break;
-    start = end + 1;
-    end = line.find_first_of(delimiter, start);
+  std::istringstream istr(line);
+  while (std::getline(istr, line, delimiter)) {
+    emptyVector.push_back(std::stof(line));
   }
 }
 
@@ -48,6 +42,7 @@ void QualityEstimator::mapBPEToWords(Response& sentence, Words words) {
   // each element of vector is a map containing the bpes of a word
   // each map key is a tuple consisting of the begining and end of the specific subword
   for (int sentenceIdx = 0; sentenceIdx < sentence.size(); sentenceIdx++) {
+    WordFeatures features;
     auto& quality = sentence.qualityScores[sentenceIdx];
     auto eos_token = words[0].DEFAULT_EOS_ID;
     QualityEstimator::SentenceQualityEstimate sentence_quality_scores;
@@ -55,8 +50,13 @@ void QualityEstimator::mapBPEToWords(Response& sentence, Words words) {
     // first_score
     sentence_quality_scores.wordByteRanges.push_back(sentence.target.wordAsByteRange(sentenceIdx, 0));
     sentence_quality_scores.wordQualitityScores.push_back(quality.word.front());
+    features.min_bpe = quality.word.front();
+    features.len_bpe = 1;
+    features.bpe_sum_over_len = quality.word.front();
+    features.overall_mean = quality.sequence / quality.word.size();
+    sentence_quality_scores.wordFeatures.push_back(features);
     // first_score
-    size_t wordIdx = 1;
+    int wordIdx = 1;
     int num_subwords = 1;
     for (int i = 1; i < quality.word.size(); i++) {
       auto& p = quality.word.at(i);
@@ -72,22 +72,55 @@ void QualityEstimator::mapBPEToWords(Response& sentence, Words words) {
         ByteRange new_word{subword_begin + 1, subword_end};
         sentence_quality_scores.wordByteRanges.push_back(new_word);
         sentence_quality_scores.wordQualitityScores.push_back(p);
+        features.min_bpe = p;
+        features.len_bpe = 1;
+        features.bpe_sum_over_len = p;
+        features.overall_mean = quality.sequence / quality.word.size();
+        sentence_quality_scores.wordFeatures.push_back(features);
         num_subwords = 1;
       } else {
         auto& current_word = sentence_quality_scores.wordByteRanges.back();
         float& current_scores = sentence_quality_scores.wordQualitityScores.back();
+        auto& current_features = sentence_quality_scores.wordFeatures.back();
+        if (current_features.min_bpe > p) {
+          current_features.min_bpe = p;
+        }
         num_subwords += 1;
+        current_features.len_bpe = num_subwords;
         ByteRange new_word{current_word.begin, subword.end};
         current_word = new_word;
         // incremental mean
         current_scores = current_scores + (p - current_scores) / num_subwords;
+        current_features.bpe_sum_over_len = current_scores;
       }
     }
     this->quality_scores_.push_back(sentence_quality_scores);
   }
 }
 
-void QualityEstimator::computeQualityScores(Response& sentence, Words words) { this->mapBPEToWords(sentence, words); }
+void QualityEstimator::predictWordLevel() {}
+
+AlignedVector<float> QualityEstimator::extractFeatutes() {
+  auto& word_scores = this->quality_scores_[0].wordFeatures;  // there will always be only one sentence
+  const intgemm::Index num_words = word_scores.size();
+  const intgemm::Index width = 16;
+  AlignedVector<float> feature_matrix(num_words * width);
+  int j = 0;
+  for (auto& it : word_scores) {
+    feature_matrix[j] = (it.bpe_sum_over_len - this->means_[0]) / this->stds_[0];
+    feature_matrix[j + 1] = (it.len_bpe - this->means_[1]) / this->stds_[1];
+    feature_matrix[j + 2] = (it.min_bpe - this->means_[2]) / this->stds_[2];
+    feature_matrix[j + 3] = (it.overall_mean - this->means_[3]) / this->stds_[3];
+    j += 4;
+  }
+  return feature_matrix;
+}
+
+void QualityEstimator::computeQualityScores(Response& sentence, Words words) {
+  this->mapBPEToWords(sentence, words);
+  this->extractFeatutes();
+  this->predictWordLevel();
+}
 
 }  // namespace bergamot
 }  // namespace marian
--- src/translator/quality_estimator.h
+++ src/translator/quality_estimator.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "annotation.h"
+#include "intgemm/intgemm.h"
 #include "response.h"
 
 namespace marian {
@@ -12,18 +13,27 @@ namespace bergamot {
 
 class QualityEstimator {
  private:
+  struct WordFeatures {
+    float bpe_sum_over_len;
+    float min_bpe;
+    float len_bpe;
+    float overall_mean;
+  };
   struct SentenceQualityEstimate {
     std::vector<float> wordQualitityScores;
     std::vector<ByteRange> wordByteRanges;
+    std::vector<WordFeatures> wordFeatures;
     float sentenceScore;
   };
-  std::vector<std::map<std::tuple<size_t, size_t>, float>> mapping;
   void initVector(std::vector<float>& emptyVector, std::string line);
-  void mapBPEToWords(Response& sentence, Words words);
-  std::vector<int> predictWordLevel(std::vector<std::vector<float>> feature_vector);
-  std::vector<SentenceQualityEstimate> quality_scores_;
-  std::vector<float> stds_, means_, coefficients_, intercept_;
   void load(AlignedMemory file_parameters);
+  std::vector<SentenceQualityEstimate> quality_scores_;
+  std::vector<float> stds_, means_;
+  std::vector<float> coefficients_, intercept_;
+
+  void mapBPEToWords(Response& sentence, Words words);
+  AlignedVector<float> extractFeatutes();
+  void predictWordLevel();
 
  public:
   explicit QualityEstimator(AlignedMemory qualityEstimatorMemory);

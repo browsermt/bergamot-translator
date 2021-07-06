37f30aaf8d075294c83f7f89abe2934011659e87
Abhishek Aggarwal <aaggarwal@mozilla.com>
2021-07-06 15:01:54 +0200

    JS bindings to return sentence byte ranges

--- wasm/bindings/response_bindings.cpp
+++ wasm/bindings/response_bindings.cpp
@@ -14,11 +14,20 @@ typedef marian::bergamot::Response Response;
 using namespace emscripten;
 
 // Binding code
+EMSCRIPTEN_BINDINGS(byte_range) {
+  value_object<marian::bergamot::ByteRange>("ByteRange")
+      .field("begin", &marian::bergamot::ByteRange::begin)
+      .field("end", &marian::bergamot::ByteRange::end);
+}
+
 EMSCRIPTEN_BINDINGS(response) {
   class_<Response>("Response")
       .constructor<>()
+      .function("size", &Response::size)
       .function("getOriginalText", &Response::getOriginalText)
-      .function("getTranslatedText", &Response::getTranslatedText);
+      .function("getTranslatedText", &Response::getTranslatedText)
+      .function("getSourceSentence", &Response::getSourceSentenceAsByteRange)
+      .function("getTranslatedSentence", &Response::getTargetSentenceAsByteRange);
 
   register_vector<Response>("VectorResponse");
 }

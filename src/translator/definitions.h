049941f407545c06e634a924aa3bf15a45f908ca
Andre Barbosa <andre.barbosa@quintoandar.com.br>
2021-07-12 03:42:06 -0300

    fix simple comments

--- src/translator/definitions.h
+++ src/translator/definitions.h
@@ -30,7 +30,7 @@ struct MemoryBundle {
   /// @todo Not implemented yet
   AlignedMemory ssplitPrefixFile{};
 
-  AlignedMemory qualityEstimatorMemory;  ///< Byte-array of shortlist (aligned to 64)
+  AlignedMemory qualityEstimatorMemory;  ///< Byte-array of qe model (aligned to 64)
 };
 
 /// ByteRange stores indices for half-interval [begin, end) in a string. Can be

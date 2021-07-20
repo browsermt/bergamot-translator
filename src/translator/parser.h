2ccc53bbe2146896f203b6721df2a846a983a1ed
Andre Barbosa <andre.barbosa@quintoandar.com.br>
2021-07-20 01:48:47 -0300

    refactor documentation and apply clang format

--- src/translator/parser.h
+++ src/translator/parser.h
@@ -29,8 +29,7 @@ inline marian::ConfigParser createConfigParser() {
   cp.addOption<std::string>("--bergamot-mode", "Bergamot Options",
                             "Operating mode for bergamot: [wasm, native, decoder]", "native");
 
-  cp.addOption<std::string>("--quality", "Bergamot Options",
-                            "File considering Quality Estimation model. Supported file extensions: .txt");
+  cp.addOption<std::string>("--quality", "Bergamot Options", "File considering Quality Estimation model");
 
   return cp;
 }

73b16c0e3229d824bf1bdd80caa7d64d0e2ea91c
Felipe C. Dos Santos <felipe.santos.k@gmail.com>
2021-07-25 03:09:19 -0300

    User QualityHelper class and fix expected results in tests

--- src/tests/units/quality_estimator_tests.cpp
+++ src/tests/units/quality_estimator_tests.cpp
@@ -4,6 +4,7 @@
 
 #include "catch.hpp"
 #include "translator/quality_estimator.h"
+#include "translator/quality_helper.h"
 
 using namespace marian::bergamot;
 
@@ -19,7 +20,7 @@ SCENARIO("Quality Estimator Test ", "[QualityEstimator]") {
         marian::string_view(target.data(), 1),       // "-"
         marian::string_view(target.data() + 1, 5),   // " Este"
         marian::string_view(target.data() + 6, 3),   // "  es"
-        marian::string_view(target.data() + 9, 3),   // " un "
+        marian::string_view(target.data() + 9, 3),   // " un"
         marian::string_view(target.data() + 12, 8),  // " ejemplo",
         marian::string_view(target.data() + 20, 1),  // ".",
         marian::string_view(target.data() + 21, 0),  // "",
@@ -34,43 +35,26 @@ SCENARIO("Quality Estimator Test ", "[QualityEstimator]") {
 
     // Memory / Features
 
-    size_t featureDims = 4;
-
     std::vector<float> stds = {0.200000003, 0.300000012, 2.5, 0.100000001};
     std::vector<float> means = {-0.100000001, -0.769999981, 5, -0.5};
     std::vector<float> coefficients = {0.99000001, 0.899999976, -0.200000003, 0.5};
     std::vector<float> intercept = {-0.300000012, -0.300000012, -0.300000012, -0.300000012};
 
-    std::vector<std::vector<float> > features = {stds, means, coefficients, intercept};
-
-    QualityEstimator::Header header = {BINARY_QE_MODEL_MAGIC, features.size() * sizeof(float)};
-
-    marian::bergamot::AlignedMemory memory(sizeof(header) + features.size() * featureDims * sizeof(float));
-
-    size_t index = 0;
-    memcpy(memory.begin(), &header, sizeof(header));
-    index += sizeof(header);
-
-    for (const auto& feature : features) {
-      for (const auto& value : feature) {
-        memcpy(memory.begin() + index, &value, sizeof(float));
-        index += sizeof(float);
-      }
-    }
+    std::vector<std::vector<float> > lrParameters = {stds, means, coefficients, intercept};
 
     AND_GIVEN("QualityEstimator") {
-      QualityEstimator qualityEstimator(std::move(memory));
+      QualityEstimator qualityEstimator(std::move(QualityHelper::writeQualityEstimatorMemory( lrParameters )));
 
       WHEN("It's call computeQualityScores") {
         auto wordsQualityEstimate = qualityEstimator.computeQualityScores(logProbs, annotatedTarget, 0);
 
         THEN("It's retuned WordsQualityEstimate") {
           CHECK(wordsQualityEstimate.wordByteRanges ==
-                std::vector<ByteRange>({{0, 0}, {2, 5}, {7, 8}, {10, 11}, {13, 21}, {21, 21}}));
+                std::vector<ByteRange>({{0, 1}, {2, 6}, {7, 9}, {10, 12}, {13, 21}}));
 
           CHECK(wordsQualityEstimate.wordQualityScores ==
-                std::vector<float>({0.883, 0.988, 0.988, 0.606, 0.952, 0.988}));
-          CHECK(wordsQualityEstimate.sentenceScore == Approx(0.90075));
+                std::vector<float>({0.883, 0.988, 0.988, 0.606, 0.952}));
+          CHECK(wordsQualityEstimate.sentenceScore == Approx( 0.88341f));
         }
       }
     }

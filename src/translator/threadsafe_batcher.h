a53c44c3527e3ec6a0dbde8f3b1e6be4ebe5b779
Jerin Philip <jphilip@ed.ac.uk>
2021-08-06 11:25:20 +0000

    Adding comments to explain the template based generalization

--- src/translator/threadsafe_batcher.h
+++ src/translator/threadsafe_batcher.h
@@ -15,6 +15,22 @@
 namespace marian {
 namespace bergamot {
 
+/// The following mechanism operates in a multithreaded async-workflow guarding access to the pushes to the structure
+/// keeping sentences bucketed by length and sorted by priority.
+///
+/// This is a wrap of a producer-consumer queue implemented as a monitor, where there is a mutex guarding the
+/// underlying data structure and (worker/consumer) threads waiting on a condition variable and the queuing thread
+/// producing and notifying waiting threads (consumers) through the same condition variable.
+///
+/// Originally written by for a single model (where items are produce: Request, consume: Batch), converted to
+/// also work for multiple models where items are produce: (TranslationModel, Request), consume: (TranlsationModel,
+/// Batch). This is accomplished by template parameter packs.
+///
+/// Requires BatchingPoolType to implement:
+///
+/// * size_t addRequest(...)
+/// * size_t generateBatch(...)
+
 template <class BatchingPoolType>
 class GuardedBatchingPoolAccess {
  public:

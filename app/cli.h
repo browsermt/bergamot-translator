7fd2a3bdddc450c07c0850037cc1357db3e93292
Jerin Philip <jphilip@ed.ac.uk>
2021-08-10 14:20:01 +0000

    Reset attempt to remove unused config

--- app/cli.h
+++ app/cli.h
@@ -47,7 +47,7 @@ void wasm(Ptr<Options> options) {
   std::string config = options->asYamlString();
   MemoryBundle memoryBundle = getMemoryBundleFromConfig(options);
   Ptr<TranslationModel> translationModel = New<TranslationModel>(config, std::move(memoryBundle));
-  BlockingService service;
+  BlockingService service(config);
 
   ResponseOptions responseOptions;
   std::vector<std::string> texts;

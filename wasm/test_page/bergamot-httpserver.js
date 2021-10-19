a0cb1e4b3d2e06027a7f979b0c66f1336a6688e9
Abhishek Aggarwal <66322306+abhi-agg@users.noreply.github.com>
2021-10-19 14:40:54 +0200

    Wasm test page UI for translating b/w non-English language pairs (#231)
    * Updated Wasm test page UI for translating b/w non-English language pairs
    * Both "from" and "to" language dropdowns now allow non-English languages

--- wasm/test_page/bergamot-httpserver.js
+++ wasm/test_page/bergamot-httpserver.js
@@ -1,6 +1,5 @@
-require(__dirname  + '/helper.js');
-
 const http = require('http');
+const https = require('https')
 const express = require('express');
 const app = express();
 const server = http.createServer(app);
@@ -8,11 +7,36 @@ const fs = require('fs');
 const url = require('url');
 const nocache = require('nocache');
 const cors = require('cors');
+const path = require('path');
 
 let port = 8000;
+if (process.argv[2]) {
+    port = process.argv[2];
+}
+
+let skipssl = 0;
+if (process.argv[3]) {
+    skipssl = process.argv[3];
+}
+
+let certpath = "/etc/letsencrypt";
+if (process.argv[4]) {
+    certpath = process.argv[4];
+}
 
 app.use(cors())
 app.use(nocache());
+
+app.get('/', cors(), function(req, res) {
+    if (!req.secure && skipssl != 1) {
+        return res.redirect("https://" + req.headers.host + req.url);
+    }
+    res.sendFile(path.join(__dirname + '/index.html'));
+    res.header('Cross-Origin-Embedder-Policy','require-corp');
+    res.header('Cross-Origin-Opener-Policy','same-origin');
+    res.header('Cross-Origin-Resource-Policy','same-origin');
+});
+
 app.get('/*.*' , cors(), function(req, res) {
     var options = url.parse(req.url, true);
     var mime = Helper.getMime(options);
@@ -33,6 +57,37 @@ function serveFile(res, pathName, mime) {
         res.end(data);
     });
 }
+
+if (skipssl != 1){
+    https.createServer({
+            key: fs.readFileSync(`${certpath}/privkey.pem`),
+            cert: fs.readFileSync(`${certpath}/cert.pem`),
+            ca: fs.readFileSync(`${certpath}/chain.pem`),
+        },
+        app
+    ).listen(443, () => {
+        console.log('Listening https port 443')
+    })
+}
+
+const Helper = {
+    types: {
+       "wasm" : "application/wasm"
+       , "js" : "application/javascript"
+       , "html" : "text/html"
+       , "htm" : "text/html"
+       , "ico" : "image/vnd.microsoft.icon"
+       , "css" : "text/css"
+    },
+    getMime: function(u) {
+        var ext = this.getExt(u.pathname).replace('.', '');
+        return this.types[ext.toLowerCase()] || 'application/octet-stream';
+    },
+    getExt: function(path) {
+        var i = path.lastIndexOf('.');
+        return (i < 0) ? '' : path.substr(i);
+    }
+};
 
 server.listen(port);
 console.log(`HTTP and BinaryJS server started on port ${port}`);

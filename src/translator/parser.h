bcbbfe129525ed2be8dbf00d2da9d412667f1d8d
Jerin Philip <jerinphilip@live.in>
2021-12-21 09:22:37 +0000

    Better command-line with isolation for both Services and co-located defaults and parsing (#252)
    * CLI Rework
    
    * Consolidate common tests, template specialize CLI
    
    * Remove remnant cache stuff
    
    * [BRT]: Run BRT with new cli
    
    * Formalizing bridge
    
    * Removing stuff from parsing and moving to TestSuite
    
    * Template includes, everything consolidating at tests
    
    * Inlining readFromStdin
    
    * Removing unnecessary headers
    
    * Checking in template implementation which was missing
    
    * Sane defaults, some catches at BRT
    
    * BRT: Install fixes
    
    * Updating marian-dev to point to main
    
    * Removing the enum indirection, using strings at one place, directly
    
    * Fix typo;
    
    * [BRT] test blocking service via native
    
    * Conservative defaults for workers and cache-mutex buckets in AsyncService
    
    * Create proper barriers for cmdline app
    
    * Build failure fixes
    
    * Moving common, common-impl to a familiar structure
    
    * Binary reorganization: async, blocking, wasm
    
    - async tests AsyncService
    - blocking tests BlockingService
    - wasm arranges tests for things that are Mozilla requirements. eg:
        - bytearray
        - multiple sentences in same translate request workflow.
    
    * [brt] updates to adapt to cli rework
    
    * [brt] updates to adapt to cli rework, all working
    
    * Empty commit, sync brt online and run GitHub CI
    
    * Switch for parser to have multiple mode or not
    
    * [brt]: Fix for --bergamot-mode being removed from CLI app
    
    * [brt]: Fix for --bergamot-mode being removed from CLI app
    
    * [brt]: Removing remnant faithful translation test from blocking/

--- src/translator/parser.h
+++ src/translator/parser.h
@@ -6,6 +6,7 @@
 
 #include "3rd_party/marian-dev/src/3rd_party/CLI/CLI.hpp"
 #include "3rd_party/yaml-cpp/yaml.h"
+#include "common/build_info.h"
 #include "common/config_parser.h"
 #include "common/config_validator.h"
 #include "common/options.h"
@@ -14,36 +15,34 @@
 namespace marian {
 namespace bergamot {
 
-enum OpMode {
-  APP_WASM,
-  APP_NATIVE,
-  APP_DECODER,
-  TEST_SOURCE_SENTENCES,
-  TEST_TARGET_SENTENCES,
-  TEST_SOURCE_WORDS,
-  TEST_TARGET_WORDS,
-  TEST_QUALITY_ESTIMATOR_WORDS,
-  TEST_QUALITY_ESTIMATOR_SCORES,
-  TEST_FORWARD_BACKWARD_FOR_OUTBOUND,
-  TEST_TRANSLATION_CACHE,
-};
-
-/// Overload for CL11, convert a read from a stringstream into opmode.
-std::istringstream &operator>>(std::istringstream &in, OpMode &mode);
-
+template <class Service>
 struct CLIConfig {
+  using ServiceConfig = typename Service::Config;
   using ModelConfigPaths = std::vector<std::string>;
-  ModelConfigPaths modelConfigPaths;
-  bool byteArray;
-  bool validateByteArray;
-  bool html;
-  size_t numWorkers;
-  OpMode opMode;
 
-  // Cache parameters
-  bool cacheEnabled{false};
-  size_t cacheSize{20};
-  size_t cacheMutexBuckets{4};
+  std::string opMode;
+
+  // For marian-models we retain the old marian-yml configs to a large extent. These are supplied as file-paths to the
+  // CLI. For multiple model workflows, we allow more than one model config to be supplied. How to process the models
+  // provided is decided by the application.
+  ModelConfigPaths modelConfigPaths;
+
+  ServiceConfig serviceConfig;
+
+  /// All config in bergamot has the following templated addOptions(...) method hierarchically placing parse actions on
+  /// "option-groups" in nested structs. This allows to keep additional documentation and information on defaults
+  /// alongside. Since this is templated with App, we don't add a CLI11 dependency in any configs, thus CLI11 not coming
+  /// into the picture until the parser is instantiated.
+  template <class App>
+  static void addOptions(App &app, CLIConfig<Service> &config, bool multiOpMode = false) {
+    if (multiOpMode) {
+      app.add_option("--bergamot-mode", config.opMode, "");
+    }
+    app.add_option("--model-config-paths", config.modelConfigPaths,
+                   "Configuration files list, can be used for pivoting multiple models or multiple model workflows");
+
+    ServiceConfig::addOptions(app, config.serviceConfig);
+  };
 };
 
 /// ConfigParser for bergamot. Internally stores config options with CLIConfig. CLI11 parsing binds the parsing code to
@@ -54,21 +53,48 @@ struct CLIConfig {
 /// configParser.parseArgs(argc, argv);
 /// auto &config = configParser.getConfig();
 /// ```
+template <class Service>
 class ConfigParser {
  public:
-  ConfigParser();
-  void parseArgs(int argc, char *argv[]);
-  const CLIConfig &getConfig() { return config_; }
+  ConfigParser(const std::string &appName, bool multiOpMode = false) : app_{appName} {
+    addSpecialOptions(app_);
+    CLIConfig<Service>::addOptions(app_, config_, multiOpMode);
+  };
+  void parseArgs(int argc, char *argv[]) {
+    try {
+      app_.parse(argc, argv);
+      handleSpecialOptions();
+    } catch (const CLI::ParseError &e) {
+      exit(app_.exit(e));
+    }
+  };
+  const CLIConfig<Service> &getConfig() { return config_; }
 
  private:
   // Special Options: build-info and version. These are not taken down further, the respective logic executed and
   // program exits after.
-  void addSpecialOptions(CLI::App &app);
-  void handleSpecialOptions();
+  void addSpecialOptions(CLI::App &app) {
+    app.add_flag("--build-info", build_info_, "Print build-info and exit");
+    app.add_flag("--version", version_, "Print version-info and exit");
+  };
 
-  void addOptionsBoundToConfig(CLI::App &app, CLIConfig &config);
+  void handleSpecialOptions() {
+    if (build_info_) {
+#ifndef _MSC_VER  // cmake build options are not available on MSVC based build.
+      std::cerr << cmakeBuildOptionsAdvanced() << std::endl;
+      exit(0);
+#else   // _MSC_VER
+      ABORT("build-info is not available on MSVC based build.");
+#endif  // _MSC_VER
+    }
 
-  CLIConfig config_;
+    if (version_) {
+      std::cerr << buildVersion() << std::endl;
+      exit(0);
+    }
+  }
+
+  CLIConfig<Service> config_;
   CLI::App app_;
 
   bool build_info_{false};
@@ -79,7 +105,7 @@ std::shared_ptr<marian::Options> parseOptionsFromString(const std::string &confi
                                                         std::string pathsInSameDirAs = "");
 std::shared_ptr<marian::Options> parseOptionsFromFilePath(const std::string &config, bool validate = true);
 
-}  //  namespace bergamot
+}  // namespace bergamot
 }  //  namespace marian
 
 #endif  //  SRC_BERGAMOT_PARSER_H

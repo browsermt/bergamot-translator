49267342a22a96e617c6a65fe224b3c808490149
Jerin Philip <jerinphilip@live.in>
2022-01-24 21:16:48 +0000

    Connecting C++ and Python sphinx (#311)
    * Remove doc for moving into python
    * Include python.rst in toc
    * Use 18.04 cp3.7 wheel
    * Integrating python documentation

--- doc/conf.py
+++ doc/conf.py
@@ -14,21 +14,23 @@ import os
 import datetime
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Bergamot Translator'
-copyright = '2021, Bergamot Translator Team'
-author = 'Bergamot Translator Team'
+project = "Bergamot Translator"
+copyright = "2021, Bergamot Translator Team"
+author = "Bergamot Translator Team"
 
 # The full version, including alpha/beta/rc tags
 # TODO: add GitHub commit hash to the version
-version_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'BERGAMOT_VERSION')
+version_file = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), "BERGAMOT_VERSION"
+)
 with open(os.path.abspath(version_file)) as f:
     version = f.read().strip()
-release = version + ' ' + str(datetime.date.today())
+release = version + " " + str(datetime.date.today())
 
 
 # -- General configuration ---------------------------------------------------
@@ -37,24 +39,26 @@ release = version + ' ' + str(datetime.date.today())
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax',
-    'sphinx.ext.todo',
-    'breathe',
-    'exhale',
-    'recommonmark',
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "breathe",
+    "exhale",
+    "recommonmark",
+    "sphinx.ext.autodoc",
+    "sphinxarg.ext",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    'build',
-    'doxygen',
-    'venv',
-    'README.md',
+    "build",
+    "doxygen",
+    "venv",
+    "README.md",
 ]
 
 
@@ -63,23 +67,23 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-htmlhelp_basename = 'bergamot-translator'
+html_theme = "sphinx_rtd_theme"
+htmlhelp_basename = "bergamot-translator"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-html_css_files = ['css/custom.css']
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # The base URL which points to the root of the HTML documentation
-html_baseurl = 'http://jerinphilip.github.io/bergamot-translator'
+html_baseurl = "http://jerinphilip.github.io/bergamot-translator"
 
 
 # -- Extension configuration -------------------------------------------------
 
-breathe_projects = { 'bergamot-translator': './doxygen/xml' }
-breathe_default_project = 'bergamot-translator'
+breathe_projects = {"bergamot-translator": "./doxygen/xml"}
+breathe_default_project = "bergamot-translator"
 
 doxygen_config = """
 INPUT                = ../src ../app
@@ -94,27 +98,28 @@ WARN_IF_UNDOCUMENTED = NO
 """
 
 exhale_args = {
-    'containmentFolder'     : './api',
-    'rootFileName'          : 'library_index.rst',
-    'rootFileTitle'         : 'Library API',
-    'doxygenStripFromPath'  : '..',
-    'createTreeView'        : True,
-    'exhaleExecutesDoxygen' : True,
-    'exhaleDoxygenStdin'    : doxygen_config.strip(),
+    "containmentFolder": "./api",
+    "rootFileName": "library_index.rst",
+    "rootFileTitle": "Library API",
+    "doxygenStripFromPath": "..",
+    "createTreeView": True,
+    "exhaleExecutesDoxygen": True,
+    "exhaleDoxygenStdin": doxygen_config.strip(),
 }
 
-primary_domain = 'cpp'
-highlight_language = 'cpp'
+primary_domain = "cpp"
+highlight_language = "cpp"
 
 # A trick to include markdown files from outside the source directory using
 # 'mdinclude'. Warning: all other markdown files not included via 'mdinclude'
 # will be rendered using recommonmark as recommended by Sphinx
 from m2r import MdInclude
 
+
 def setup(app):
     # from m2r to make `mdinclude` work
-    app.add_config_value('no_underscore_emphasis', False, 'env')
-    app.add_config_value('m2r_parse_relative_links', False, 'env')
-    app.add_config_value('m2r_anonymous_references', False, 'env')
-    app.add_config_value('m2r_disable_inline_math', False, 'env')
-    app.add_directive('mdinclude', MdInclude)
+    app.add_config_value("no_underscore_emphasis", False, "env")
+    app.add_config_value("m2r_parse_relative_links", False, "env")
+    app.add_config_value("m2r_anonymous_references", False, "env")
+    app.add_config_value("m2r_disable_inline_math", False, "env")
+    app.add_directive("mdinclude", MdInclude)

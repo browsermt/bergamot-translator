8a078ffed644747529eb51311d7524d4ce6cb072
Jerin Philip <jphilip@ed.ac.uk>
2022-01-25 15:14:56 +0000

    python files consistent styling - include doc/conf.py

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -5,9 +5,7 @@ from .repository import Aggregator, TranslateLocallyLike
 
 REPOSITORY = Aggregator(
     [
-        TranslateLocallyLike(
-            "browsermt", "https://translatelocally.com/models.json"
-        ),
+        TranslateLocallyLike("browsermt", "https://translatelocally.com/models.json"),
         TranslateLocallyLike(
             "opus", "https://object.pouta.csc.fi/OPUS-MT-models/app/models.json"
         ),

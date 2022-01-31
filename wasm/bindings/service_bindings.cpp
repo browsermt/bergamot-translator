779f8a31ea9da0c6dc1278edd7825e9c83f63653
Abhishek Aggarwal <aaggarwal@mozilla.com>
2022-01-31 18:08:30 +0100

    Wasm/JS: Pivot translation API JS binding and test page update

--- wasm/bindings/service_bindings.cpp
+++ wasm/bindings/service_bindings.cpp
@@ -78,7 +78,8 @@ EMSCRIPTEN_BINDINGS(blocking_service_config) {
 EMSCRIPTEN_BINDINGS(blocking_service) {
   class_<BlockingService>("BlockingService")
       .constructor<BlockingService::Config>()
-      .function("translate", &BlockingService::translateMultiple);
+      .function("translate", &BlockingService::translateMultiple)
+      .function("translateViaPivoting", &BlockingService::pivotMultiple);
 
   register_vector<std::string>("VectorString");
 }

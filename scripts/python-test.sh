f8f06bef16fb0c7f2da3c69da4b7a736c53ff30f
Jerin Philip <jerinphilip@live.in>
2022-02-18 20:24:26 +0000

    Consolidate python runs into a bash script, use it at github workflow

--- scripts/python-test.sh
+++ scripts/python-test.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+set -eo pipefail;
+
 function run_coverage {
     SOURCE_DIR=$(python3 -c 'import bergamot as _; print(_.__path__[0])')
     coverage run -a --source ${SOURCE_DIR} -m bergamot ls
     coverage run -a --source ${SOURCE_DIR} -m bergamot download # Download all models
     coverage run -a --source ${SOURCE_DIR} -m bergamot translate -m en-de-tiny <<< "Hello World"
     coverage run -a --source ${SOURCE_DIR} -m bergamot translate -m en-de-tiny de-en-tiny <<< "Hello World"
+    coverage run -a --source ${SOURCE_DIR} -m bergamot download -r opus -m eng-fin-tiny # Download specific model from opus
+    coverage run -a --source ${SOURCE_DIR} -m bergamot translate --model eng-fin-tiny --repository opus <<< "Hello World"
+    coverage run -a --source ${SOURCE_DIR} -m bergamot download -m eng-fin-tiny -r opus
+    coverage run -a --source ${SOURCE_DIR} -m bergamot ls -r opus
     coverage run -a --source ${SOURCE_DIR} -m pytest --pyargs bergamot -rP
 }
 
 coverage erase
+
 run_coverage # Run once.
 run_coverage # Run again, to activate a few else paths.
-coverage report
+
 coverage html
 

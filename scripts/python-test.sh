fdb6d6bfd4702a1c68c99293ac600cc55b7077c3
Jerin Philip <jerinphilip@live.in>
2022-02-18 20:18:26 +0000

    Add a coverage script

---
+++ scripts/python-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function run_coverage {
+    SOURCE_DIR=$(python3 -c 'import bergamot as _; print(_.__path__[0])')
+    coverage run -a --source ${SOURCE_DIR} -m bergamot ls
+    coverage run -a --source ${SOURCE_DIR} -m bergamot download # Download all models
+    coverage run -a --source ${SOURCE_DIR} -m bergamot translate -m en-de-tiny <<< "Hello World"
+    coverage run -a --source ${SOURCE_DIR} -m bergamot translate -m en-de-tiny de-en-tiny <<< "Hello World"
+    coverage run -a --source ${SOURCE_DIR} -m pytest --pyargs bergamot -rP
+}
+
+coverage erase
+run_coverage # Run once.
+run_coverage # Run again, to activate a few else paths.
+coverage report
+coverage html
+

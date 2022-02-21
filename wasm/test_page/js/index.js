922c2d1b2520a057ea7edfccc36df2cb90e3e0da
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-02-21 22:52:07 +0000

    Add example of sentence and word quality output to test page

--- wasm/test_page/js/index.js
+++ wasm/test_page/js/index.js
@@ -38,22 +38,48 @@ const _prepareTranslateOptions = (paragraphs) => {
   return translateOptions;
 };
 
+const textToHTML = (text) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(text));
+  return div.innerHTML;
+};
+
 const translateCall = () => {
-  const text = document.querySelector("#input").value + "  ";
+  const text = document.querySelector("#input").value;
   if (!text.trim().length) return;
 
-  const paragraphs = text.split("\n");
+  const paragraphs = [textToHTML(text)]; // escape HTML 
   const translateOptions = _prepareTranslateOptions(paragraphs);
-  $("#output").setAttribute("disabled", true);
   const lngFrom = langFrom.value;
   const lngTo = langTo.value;
   worker.postMessage(["translate", lngFrom, lngTo, paragraphs, translateOptions]);
 };
 
+const addQualityClasses = (root) => {
+  // You can do this wit CSS variables, calc() and min/max, but JS is just easier
+
+  root.querySelectorAll('[x-bergamot-sentence-score]').forEach(el => {
+    // Note: these thresholds are just examples, they are not good thresholds!  
+    el.classList.toggle('bad', parseFloat(el.getAttribute('x-bergamot-sentence-score')) > -0.1);
+  });
+
+  root.querySelectorAll('[x-bergamot-word-score]').forEach(el => {
+    // Note: these thresholds are just examples, they are not good thresholds!
+    el.classList.toggle('bad', parseFloat(el.getAttribute('x-bergamot-word-score')) > -0.1);
+  });
+
+  // Add tooltips to each (sub)word with sentence and word score.
+  root.querySelectorAll('[x-bergamot-sentence-score] > [x-bergamot-word-score]').forEach(el => {
+    const sentenceScore = parseFloat(el.parentNode.getAttribute('x-bergamot-sentence-score'));
+    const wordScore = parseFloat(el.getAttribute('x-bergamot-word-score'));
+    el.title = `Sentence: ${sentenceScore}  Word: ${wordScore}`;
+  });
+}
+
 worker.onmessage = function (e) {
   if (e.data[0] === "translate_reply" && e.data[1]) {
-    document.querySelector("#output").value = e.data[1].join("\n\n");
-    $("#output").removeAttribute("disabled");
+    document.querySelector("#output").innerHTML = e.data[1];
+    addQualityClasses(document.querySelector("#output"));
   } else if (e.data[0] === "load_model_reply" && e.data[1]) {
     status(e.data[1]);
     translateCall();
@@ -76,8 +102,8 @@ const loadModel = () => {
     console.log(`Loading model '${lngFrom}${lngTo}'`);
     worker.postMessage(["load_model", lngFrom, lngTo]);
   } else {
-    const input = document.querySelector("#input").value;
-    document.querySelector("#output").value = input;
+    const input = textToHTML(document.querySelector("#input").value);
+    document.querySelector("#output").innerHTML = input;
   }
 };
 
